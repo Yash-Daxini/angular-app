3e3747727c9a89d0b5fe4f19446f026ecba2bd63
Infinity <yashdaxini2003@gmail.com>
2025-04-24 09:09:26 +0530

    Started component creation

---
+++ src/components/Title/title.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'title',
+    templateUrl: './title.html',
+    styleUrl: './title.css'
+})
+export class AppComponent{
+    title = 'title'
+}
